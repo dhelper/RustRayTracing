1f1cac54d8e316b9fba58d3e20c75f722a85af79
Dror Helper <dror.helper@gmail.com>
2021-01-11 21:35:06 +0200

    Started format as PPM

--- src/canvas.rs
+++ src/canvas.rs
@@ -8,6 +8,27 @@ pub struct Canvas {
 }
 
 impl Canvas {
+    pub(crate) fn to_ppm(&self) -> String {
+        let mut res = String::from("P3\n80 40\n255\n");
+
+        for h in 0..self.height() {
+            for w in 0..self.width() {
+                let red = (self.pixels[[w, h]].red * 256.0) as u8;
+                let green = (self.pixels[[w, h]].green * 256.0) as u8;
+                let blue = (self.pixels[[w, h]].blue * 256.0) as u8;
+
+                res = res + &red.to_string() + " " + &green.to_string() + " " + &blue.to_string() + " ";
+            }
+            res.push_str("\n");
+        }
+
+        return res;
+    }
+
+    pub(crate) fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
+        self.pixels[[x, y]] = color;
+    }
+
     pub fn new(width: usize, height: usize) -> Self {
         Canvas { pixels: Array2::<Color>::default((width, height)) }
     }
@@ -15,11 +36,11 @@ impl Canvas {
     pub fn width(&self) -> usize {
         return self.pixels.shape()[0];
     }
+
     pub fn height(&self) -> usize {
         return self.pixels.shape()[1];
     }
 }
-
 
 
 #[cfg(test)]
@@ -36,8 +57,48 @@ mod tests {
 
         for w in 0..c.width() {
             for h in 0..c.height() {
-                assert_eq!(Color { red: 0.0, green: 0.0, blue: 0.0 }, c.pixels[[w,h]]);
+                assert_eq!(Color { red: 0.0, green: 0.0, blue: 0.0 }, c.pixels[[w, h]]);
             }
         }
+    }
+
+    #[test]
+    fn writing_pixels_to_a_canvas() {
+        let mut c = Canvas::new(10, 20);
+        let red = Color { red: 1.0, blue: 0.0, green: 0.0 };
+
+        c.write_pixel(2, 3, red);
+
+        assert_eq!(red, c.pixels[[2, 3]]);
+    }
+
+    #[test]
+    fn constructing_the_ppm_header() {
+        let c = Canvas::new(5, 3);
+
+        let ppm = c.to_ppm();
+
+        assert!(ppm.starts_with("P3\n80 40\n255\n"));
+    }
+
+    #[test]
+    fn constructing_the_ppm_pixel_data() {
+        let mut c = Canvas::new(5, 3);
+
+        let c1 = Color { red: 1.5, green: 0.0, blue: 0.0 };
+        let c2 = Color { red: 0.0, green: 0.5, blue: 0.0 };
+        let c3 = Color { red: -0.5, green: 0.0, blue: 1.0 };
+
+        c.write_pixel(0, 0, c1);
+        c.write_pixel(2, 1, c2);
+        c.write_pixel(4, 2, c3);
+
+        let ppm = c.to_ppm();
+
+        let lines: Vec<&str> = ppm.lines().collect();
+
+        assert_eq!("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", lines[3].trim());
+        assert_eq!("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0", lines[4].trim());
+        assert_eq!("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", lines[5].trim());
     }
 }
