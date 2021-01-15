8eb1ef0fc20faba2d1fa79fa53f03d3404cb9afb
Dror Helper <dror.helper@gmail.com>
2021-01-15 14:11:35 +0200

    Added line breaks

--- src/canvas.rs
+++ src/canvas.rs
@@ -12,17 +12,36 @@ impl Canvas {
         let mut res = String::from("P3\n80 40\n255\n");
 
         for h in 0..self.height() {
+            let mut line_counter: usize = 0;
             for w in 0..self.width() {
                 let red = (self.pixels[[w, h]].red * 256.0) as u8;
                 let green = (self.pixels[[w, h]].green * 256.0) as u8;
                 let blue = (self.pixels[[w, h]].blue * 256.0) as u8;
 
-                res = res + &red.to_string() + " " + &green.to_string() + " " + &blue.to_string() + " ";
+                line_counter = Canvas::add_color_to_file(&mut res, line_counter, red);
+                line_counter = Canvas::add_color_to_file(&mut res, line_counter, green);
+                line_counter = Canvas::add_color_to_file(&mut res, line_counter, blue);
             }
             res.push_str("\n");
         }
 
         return res;
+    }
+
+    fn add_color_to_file(res: &mut String, line_counter: usize, color: u8) -> usize {
+        let mut local_counter = line_counter;
+        let color_string = color.to_string();
+        let local_res:String = res.to_string();
+
+        if color_string.len() + local_counter > 70 {
+            *res = format!("{}\n{}", local_res, color_string);
+            local_counter = color_string.len();
+        } else {
+            *res = format!("{} {}", local_res, color_string);
+            local_counter = local_counter + 1 + color_string.len();
+        }
+
+        return local_counter;
     }
 
     pub(crate) fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
@@ -100,5 +119,24 @@ mod tests {
         assert_eq!("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0", lines[3].trim());
         assert_eq!("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0", lines[4].trim());
         assert_eq!("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255", lines[5].trim());
+    }
+
+    #[test]
+    fn splitting_long_lines_in_ppm_files() {
+        let mut c = Canvas::new(10, 2);
+        for w in 0..c.width() {
+            for h in 0..c.height() {
+                c.write_pixel(w, h, Color { red: 1.0, green: 0.8, blue: 0.6 })
+            }
+        }
+
+        let ppm = c.to_ppm();
+
+        let lines: Vec<&str> = ppm.lines().collect();
+
+        assert_eq!("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", lines[3].trim());
+        assert_eq!("153 255 204 153 255 204 153 255 204 153 255 204 153", lines[4].trim());
+        assert_eq!("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204", lines[5].trim());
+        assert_eq!("153 255 204 153 255 204 153 255 204 153 255 204 153", lines[6].trim());
     }
 }
