a8b2bbed5b312f2db4fa16d6cb6a223194b7faca
Dror Helper <dror.helper@gmail.com>
2021-01-18 19:43:55 +0200

    Added macro for creating matrices and added debug and EQ traits

--- src/matrix.rs
+++ src/matrix.rs
@@ -2,46 +2,27 @@ use std::ops::Index;
 
 #[macro_export]
 macro_rules! matrix {
-    ($n:expr) =>{
-       [[f64; $n]; $n]
+    ($name:ident, $n:expr) =>
+    {
+        #[derive(Debug)]
+        #[derive(PartialEq)]
+        pub struct $name {
+            values: [[f64; $n]; $n]
+        }
+
+        impl Index<(usize, usize)> for $name {
+            type Output = f64;
+
+            fn index(&self, index: (usize, usize)) -> &Self::Output {
+                &self.values[index.0][index.1]
+            }
+        }
     }
 }
 
-pub struct Matrix4 {
-    values: [[f64; 4]; 4]
-}
-
-pub struct Matrix3 {
-    values: [[f64; 3]; 3]
-}
-
-pub struct Matrix2 {
-    values: [[f64; 2]; 2]
-}
-
-impl Index<(usize, usize)> for Matrix4 {
-    type Output = f64;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.values[index.0][index.1]
-    }
-}
-
-impl Index<(usize, usize)> for Matrix3 {
-    type Output = f64;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.values[index.0][index.1]
-    }
-}
-
-impl Index<(usize, usize)> for Matrix2 {
-    type Output = f64;
-
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.values[index.0][index.1]
-    }
-}
+matrix!(Matrix4, 4);
+matrix!(Matrix3, 3);
+matrix!(Matrix2, 2);
 
 #[cfg(test)]
 mod tests {
@@ -51,27 +32,23 @@ mod tests {
 
     #[test]
     fn constructing_an_inspecting_a_4x4_matrix() {
-        let mat: [[f64; 4]; 4] = [
-            [1.0, 2.0, 3.0, 4.0],
-            [5.5, 6.5, 7.5, 8.5],
-            [9.0, 10.0, 11.0, 12.0],
-            [13.5, 14.5, 15.5, 16.5]
-        ];
-        let m: matrix!(4) =
-            [
+        let m = Matrix4
+        {
+            values: [
                 [1.0, 2.0, 3.0, 4.0],
                 [5.5, 6.5, 7.5, 8.5],
                 [9.0, 10.0, 11.0, 12.0],
                 [13.5, 14.5, 15.5, 16.5]
-            ];
+            ]
+        };
 
-        assert_eq!(1.0, m[0][0]);
-        assert_eq!(4.0, m[0][3]);
-        assert_eq!(5.5, m[1][0]);
-        assert_eq!(7.5, m[1][2]);
-        assert_eq!(11.0, m[2][2]);
-        assert_eq!(13.5, m[3][0]);
-        assert_eq!(15.5, m[3][2]);
+        assert_eq!(1.0, m[(0, 0)]);
+        assert_eq!(4.0, m[(0, 3)]);
+        assert_eq!(5.5, m[(1, 0)]);
+        assert_eq!(7.5, m[(1, 2)]);
+        assert_eq!(11.0, m[(2, 2)]);
+        assert_eq!(13.5, m[(3, 0)]);
+        assert_eq!(15.5, m[(3, 2)]);
     }
 
     #[test]
@@ -102,5 +79,55 @@ mod tests {
         assert_eq!(-3.0, m[(0, 0)]);
         assert_eq!(-2.0, m[(1, 1)]);
         assert_eq!(1.0, m[(2, 2)]);
+    }
+
+    #[test]
+    fn matrix_equality_with_identical_matrices() {
+        let m1 = Matrix4
+        {
+            values: [
+                [1.0, 2.0, 3.0, 4.0],
+                [5.5, 6.5, 7.5, 8.5],
+                [9.0, 10.0, 11.0, 12.0],
+                [13.5, 14.5, 15.5, 16.5]
+            ]
+        };
+
+        let m2 = Matrix4
+        {
+            values: [
+                [1.0, 2.0, 3.0, 4.0],
+                [5.5, 6.5, 7.5, 8.5],
+                [9.0, 10.0, 11.0, 12.0],
+                [13.5, 14.5, 15.5, 16.5]
+            ]
+        };
+
+        assert_eq!(m1, m2);
+    }
+
+    #[test]
+    fn matrix_equality_with_different_matrices() {
+        let m1 = Matrix4
+        {
+            values: [
+                [1.0, 2.0, 3.0, 4.0],
+                [5.5, 6.5, 7.5, 8.5],
+                [9.0, 10.0, 11.0, 12.0],
+                [13.5, 14.5, 15.5, 16.5]
+            ]
+        };
+
+        let m2 = Matrix4
+        {
+            values: [
+                [1.0, 2.0, 3.0, 4.0],
+                [5.5, 6.5, 7.5, 8.5],
+                [1.0, 2.0, 3.0, 4.0],
+                [5.5, 6.5, 7.5, 8.5]
+            ]
+        };
+
+        assert_ne!(m1, m2);
     }
 }
